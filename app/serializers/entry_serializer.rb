351657db96a9ea0d46af645504f286c2abfca762
saphron <saphron.scul@gmail.com>
2015-11-16 20:28:54 -0500

    added :id to entry serializer

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,4 +1,4 @@
 #
 class EntrySerializer < ActiveModel::Serializer
-  attributes :day_rating, :pain_rank, :note_entry, :symptoms, :medication, :mood, :created_at
+  attributes :id, :day_rating, :pain_rank, :note_entry, :symptoms, :medication, :mood, :created_at
 end
